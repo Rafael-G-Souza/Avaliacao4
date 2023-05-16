6303fdabb7d7eabd25ec815fefd9848d54f4d75f
Rafael-G-Souza <133234731+Rafael-G-Souza@users.noreply.github.com>
2023-05-16 11:50:04 -0300

    v3.0
    Avaliacao4

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var telaMenorBotao = document.getElementById("telaMenorBotao")
+var telaMenor = document.getElementById("telaMenor")
+var fecharTelaMenor = document.getElementById("fecharTelaMenor")
+
+telaMenorBotao.addEventListener("click", showMenu)
+fecharTelaMenor.addEventListener("click", closeMenu)
+
+function showMenu(){
+    telaMenorBotao.style.display = "none"
+    telaMenor.style.display = "flex"
+    telaMenor.style.flexFlow = "column"
+    telaMenor.style.transition = "1s"
+}
+function closeMenu(){
+    telaMenor.style.display = "none"
+    telaMenorBotao.style.display = "flex"
+
+}
